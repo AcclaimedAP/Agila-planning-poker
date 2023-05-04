98f2a15ac59c85071fdccec27eb911cbd58088d3
Nicholas Peters <peters.nicholas@gmail.com>
2023-05-04 11:26:15 +0200

    create and connect to database
    Co-authored-by: Alexander-Pressfelt <alexander.pressfelt@medieinstitutet.se>
    Co-authored-by: Filip-Hedlén <filip.hedlen@medieinstitutet.se>
    Co-authored-by: David-Stålgren <david.stalgren@medieinstitutet.se>

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,34 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mysql = require("mysql2");
+// eslint-disable-next-line import/no-extraneous-dependencies
+require("dotenv").config();
 
+const cors = require("cors");
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
+app.use(cors());
+
+const { DB_PORT } = process.env;
+
+app.locals.con = mysql.createConnection({
+  host: "localhost",
+  port: DB_PORT,
+  user: "billy",
+  password: "billyspw",
+  database: "planning-poker-billy",
+});
+
+app.get("/", (req, res) => {
+  const sql = "SELECT * FROM users";
+  app.locals.con.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send(result);
+  });
+});
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -15,6 +38,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 module.exports = app;
