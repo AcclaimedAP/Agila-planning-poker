a5274a1959f205db25a7d7e6797e091b390198ab
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2023-05-04 13:55:07 +0200

    Revert "Merge branch 'main' of https://github.com/Medieinstitutet/fed22d-agila-planning-poker-billy"
    This reverts commit 533c55b93ceb322525bd3331e04e34da6b7646b2, reversing
    changes made to a0ca10af956d3c5d9725bd42360c766378a7b390.

--- backend/app.js
+++ backend/app.js
@@ -2,52 +2,11 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const mysql = require("mysql2");
-const cors = require("cors");
-
-const app = express();
-const server = require("http").createServer(app);
-// eslint-disable-next-line import/no-extraneous-dependencies
-require("dotenv").config();
-
-server.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
-
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
 
 const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-app.use(cors());
-
-const { DB_PORT } = process.env;
-
-app.locals.con = mysql.createConnection({
-  host: "localhost",
-  port: DB_PORT,
-  user: "billy",
-  password: "billyspw",
-  database: "planning-poker-billy",
-});
-
-app.get("/", (req, res) => {
-  const sql = "SELECT * FROM users";
-  app.locals.con.query(sql, (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    res.send(result);
-  });
-});
-
-io.on("connection", (socket) => {
-  console.log("Connected User");
-  console.log(socket.id);
-});
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -56,5 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
