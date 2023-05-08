7aec673fcf7f30546f8eaea5d0bedefd7a3cd3d0
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2023-05-08 14:24:34 +0200

    :sparkles: add 'task add form'
    Co-authored-by: Nicholas-Peters <peters.nicholas@gmail.com>

--- frontend/src/modules/admin.ts
+++ frontend/src/modules/admin.ts
@@ -1,22 +1,66 @@
-const app = document.getElementById('app');
+import { io } from "socket.io-client";
 
-export function adminInit() {
+const app: HTMLElement | null = document.getElementById('app');
 
-    if(app) app.innerHTML = '';
+const socket = io('http://localhost:3000')
 
-    const adminSection: HTMLElement = document.createElement('section');
-    adminSection.classList.add('admin__section');
-
-    app?.append(adminSection);
-
-    const adminSectionEl = document.querySelector('.admin__section')
+export function renderAddTaskBtn(): HTMLButtonElement {
 
     const addTaskBtn: HTMLButtonElement = document.createElement('button');
     addTaskBtn.innerText = 'Add Task';
     addTaskBtn.classList.add('admin__section--addTaskBtn');
-
-    adminSectionEl?.append(addTaskBtn);
-
     addTaskBtn.addEventListener('click', renderAddTaskForm);
+
+    return addTaskBtn;
 }
 
+export function renderAddTaskForm() {
+
+    console.log('btn was clicked');
+      
+    const addTaskContainer: HTMLDivElement = document.createElement('div');
+
+    const titleLabel: HTMLLabelElement = document.createElement('label');
+    titleLabel.innerHTML = 'Title: ';
+    const descriptionLabel: HTMLLabelElement = document.createElement('label');
+    descriptionLabel.innerHTML = 'Description: ';
+    const titleInput: HTMLInputElement = document.createElement('input');
+    titleInput.id = 'titleInputElement';
+    const descriptionInput: HTMLTextAreaElement = document.createElement('textarea');
+    descriptionInput.id = 'descriptionInputElement';
+    const cancelAddTaskBtn = document.createElement('button');
+    cancelAddTaskBtn.innerText = 'Cancel';
+    cancelAddTaskBtn.id = 'cancelAddTaskBtn';
+    const confirmAddTaskBtn = document.createElement('button');
+    confirmAddTaskBtn.innerText = 'Add';
+    confirmAddTaskBtn.id = 'confirmAddTaskBtn';
+
+    cancelAddTaskBtn.addEventListener('click', () => {
+        closeAddTaskForm(addTaskContainer)
+    });
+
+    confirmAddTaskBtn.addEventListener('click', () => {
+        addTask(titleInput.value, descriptionInput.value)
+    })
+
+    addTaskContainer.append(titleLabel, titleInput, descriptionLabel, descriptionInput, cancelAddTaskBtn, confirmAddTaskBtn);
+
+    app?.appendChild(addTaskContainer);
+
+}
+
+function closeAddTaskForm(addTaskContainer: HTMLDivElement) {
+
+    app?.removeChild(addTaskContainer)
+}
+
+function addTask(title: String, description: String) {
+
+    const task = {
+        'taskTitle': title,
+        'taskDescription': description
+    }
+    console.log('task', task);
+    
+    socket.emit('add-task', task)
+}
