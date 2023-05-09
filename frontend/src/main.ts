fcc4aa9fcd95d84362d5c787b523d8dbd583e6f8
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2023-05-09 09:48:25 +0200

    Fixed socket.io with connected users in Front and Back end

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,21 @@
 import { io } from "socket.io-client";
-import { createLoginForm } from "./loginForm";
+import { createLoginForm } from "./modules/loginForm";
 import { renderAddTaskBtn } from "./modules/admin";
 import { ITask } from "./models/ITask";
 
-const socket = io('http://localhost:3000');
+export const socket = io('http://localhost:3000');
 const app: HTMLElement | null = document.getElementById('app');
 
 socket.on('connect', () => {
   console.log('connected', socket.id);
+  socket.on('user-connect', (arg) => {
+    console.log(arg);
+    });
+
+  const username = sessionStorage.getItem("username");
+  if (username) {
+    socket.emit("user-connect", username);
+  }
 });
 
 app?.appendChild(createLoginForm());
